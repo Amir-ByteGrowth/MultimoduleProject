5d05092735708c8c52b1f3deb7a8a531ebfba13c
Amir javeed <amir@vaultspay.ae>
2024-09-26 14:44:40 +0400

    all gradle formatted for vaultspay

--- merchantside/build.gradle.kts
+++ merchantside/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-//    id("com.android.library")
-//    id("org.jetbrains.kotlin.android")
-//    id("dagger.hilt.android.plugin")
-//    kotlin("kapt")
+
     `android-library`
     `kotlin-android`
 }
@@ -13,33 +10,6 @@ android {
     namespace = "com.example.featureb"
 
 }
-
-//compileSdk = 34
-//
-//defaultConfig {
-//    minSdk = 24
-//
-//    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//    consumerProguardFiles("consumer-rules.pro")
-//}
-//
-//buildTypes {
-//    release {
-//        isMinifyEnabled = false
-//        proguardFiles(
-//            getDefaultProguardFile("proguard-android-optimize.txt"),
-//            "proguard-rules.pro"
-//        )
-//    }
-//}
-//compileOptions {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//    targetCompatibility = JavaVersion.VERSION_1_8
-//}
-//kotlinOptions {
-//    jvmTarget = "1.8"
-//}
-
 dependencies {
 
     implementation(libs.androidx.core.ktx)
